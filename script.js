fa4eca07a3cf8333afb221dd598e02ba14827a31
Michaela Wagner <mickywagner@gmail.com>
2019-07-19 14:24:28 -0600

    Add buttons and eventlistener with func to new grid size button

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
 let container = document.querySelector('#container')
 
-        function createGrid(rows, cols) {
-            for (let i = 0; i < rows; i++) {
-                for (let j = 0; j < cols; j++) {
-                    let div = document.createElement('div')
-                    div.classList.add('box')
-                    container.appendChild(div)
-                    container.style.gridTemplateRows = `repeat(${rows}, 1fr)`
-                    container.style.gridTemplateColumns = `repeat(${cols}, 1fr)`
-                }
-            }
+function createGrid(rows, cols) {
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            let div = document.createElement('div')
+            div.classList.add('box')
+            container.appendChild(div)
+            container.style.gridTemplateRows = `repeat(${rows}, 1fr)`
+            container.style.gridTemplateColumns = `repeat(${cols}, 1fr)`
         }
+    }
+}
 
+function removeGrid() {
+    let boxGrid = document.querySelectorAll('.box')
+    for (let i = 0; i < boxGrid.length; i++) {
+        container.removeChild(boxGrid[i])
+    }
+}
 
+function drawBW() {
+    let boxes = document.querySelectorAll('.box')
+    boxes.forEach(div => div.addEventListener('mouseover', function (e) {
+        e.target.style.background = 'black'
+    }))
+}
 
-        function removeGrid() {
-            let boxGrid = document.querySelectorAll('.box')
-            for (let i = 0; i < boxGrid.length; i++) {
-                container.removeChild(boxGrid[i])
-            }
-        }
+let newGridBtn = document.querySelector('#newGrid') 
+newGridBtn.addEventListener('click', () => {
+    removeGrid();
+    let input = prompt('Choose a number 1-99')
+    let regex = /^(\d?[1-9]|[1-9]0)$/m
+    if (!regex.test(input)){
+        alert('Choose a valid whole number 1-99')
+        input = prompt('Choose a number 1-99')
+    }
+    createGrid(input, input)
+    drawBW()
+})
 
-        function drawBW() {
-            let boxes = document.querySelectorAll('.box')
-            boxes.forEach(div => div.addEventListener('mouseover', function (e) {
-                e.target.style.background = 'black'
-            }))
-        }
-
-        createGrid(6, 6)
-        drawBW()
+createGrid(16, 16)
+drawBW()
